8c43f17218558d64a48a61910e70558b719a33d9
luisfeliz12 <34288563+luisfeliz12@users.noreply.github.com>
2017-12-05 18:24:34 -0500

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,180 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Harry Potter and The Deathly Hallows",
+	"img" => "img/media/HPB.jpg",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "author" => [
+        "J. K. Rowling",
+    ],
+    "publisher" => "
+		Bloomsbury (UK) (Canada 2010–present)
+		Arthur A. Levine/
+		Scholastic (US)
+		Raincoast (Canada 1998–2010)",
+    "isbn" => '0-545-01022-5'
+];
+$catalog[102] = [
+    "title" => "My Bloody Life: The Making of a Latin King",
+    "img" => "img/media/TMOALK.jpg",
+    "genre" => "Biography, Autobiography",
+    "format" => "Ebook",
+    "year" => 2000,
+    "category" => "Books",
+    "authors" => [
+        "Reymundo Sanchez"
+    ],
+    "publisher" => "Chicago Review Press",
+    "isbn" => '9781569762325'
+];
+$catalog[103] = [
+    "title" => "The Autobiography of Malcolm X",
+    "img" => "img/media/mx.jpg",
+    "genre" => " Autobiography, Biography",
+    "format" => "Hardcover",
+    "year" => 1992,
+    "category" => "Books",
+    "authors" => [
+        "Malcolm X",
+        "Alex Haley"
+    ],
+    "publisher" => "Random House Publishing Group",
+    "isbn" => '9780345379757
+'
+];
+$catalog[104] = [
+    "title" => "Behold A Pale White Horse",
+    "img" => "img/media/BAPWH.jpg",
+    "genre" => "Information",
+    "format" => "Audio",
+    "year" => 1991,
+    "category" => "Books",
+    "authors" => [
+        "Milton William Cooper"
+    ],
+    "publisher" => "	Light Technology Publishing, LLC",
+    "isbn" => '9780929385228'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Forrest Gump",
+    "img" => "img/media/forest_gump.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Winston Groom",
+        "Eric Roth"
+    ],
+    "stars" => [
+        "Tom Hanks",
+        "Rebecca Williams",
+        "Sally Field",
+        "Michael Conner Humphreys"
+    ]
+];
+$catalog[202] = [
+    "title" => "Harry Potte and The Order Of The Phoneix",
+    "img" => "img/media/HP5.jpg",
+    "genre" => "Fantasy",
+    "format" => "Blu-ray",
+    "year" => 2007,
+    "category" => "Movies",
+    "director" => "David Yates",
+    "writers" => [
+        "J. K. Rowling"
+    ],
+    "stars" => [
+        "Daniel Radcliffe",
+        "Rupert Grint",
+        "Emma Watson",
+        "Helena Bonham Carter",
+        "Robbie Coltrane",
+        "Warwick Davis"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Lord of the Rings: The Fellowship of the Ring",
+    "img" => "img/media/lotr.jpg",
+    "genre" => "Drama",
+    "format" => "Blu-ray",
+    "year" => 2001,
+    "category" => "Movies",
+    "director" => "Peter Jackson",
+    "writers" => [
+        "J.R.R. Tolkien",
+        "Fran Walsh",
+        "Philippa Boyens",
+        "Peter Jackson"
+    ],
+    "stars" => [
+        "Ron Livingston",
+        "Jennifer Aniston",
+        "David Herman",
+        "Ajay Naidu",
+        "Diedrich Bader",
+        "Stephen Root"
+    ]
+];
+$catalog[204] = [
+    "title" => "X-Men: Days of Future Past",
+    "img" => "img/media/XMEN.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2014,
+    "category" => "Movies",
+    "director" => "	Bryan Singer",
+    "writers" => [
+        "William Goldman"
+    ],
+    "stars" => [
+			"Hugh Jackman",
+			"James McAvoy",
+			"Michael Fassbender",
+			"Jennifer Lawrence",
+			"Halle Berry"
+		]
+];
+//Music
+$catalog[301] = [
+    "title" => "Man on the Moon: The End of Day",
+    "img" => "img/media/motm.jpg",
+    "genre" => "Hip hop music",
+    "format" => "CD",
+    "year" => 2009,
+    "category" => "Music",
+    "artist" => "Kid Cudi"
+];
+$catalog[302] = [
+    "title" => "Passion, Pain & Demon Slayin",
+    "img" => "img/media/PPDS.jpg",
+    "genre" => "Hip-Hop",
+    "format" => "Vinyl",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Kid Cudi"
+];
+$catalog[303] = [
+    "title" => "Birds in the Trap Sing McKnight",
+    "img" => "img/media/TS.jpg",
+    "genre" => "Hip-Hop",
+    "format" => "CD",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Travis Scott"
+];
+$catalog[304] = [
+    "title" => "Flower Boy",
+    "img" => "img/media/sunflower.jpg",
+    "genre" => "Alternative hip hop",
+    "format" => "CD",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Tyler, The Creator"
+];
---
+++ My Online Library/inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>
+				<li><a href="https://www.instagram.com/gnawluis/">Instagram</a></li>
+				<li><a href="https://www.instagram.com/lupics_f">Instagram</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
